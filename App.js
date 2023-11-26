b99f02929fec1f26da33fcd29c3cac81b18148ec
Gabriel Costa de Moraes <gabrielcostademoraes13@gmail.com>
2023-11-26 18:11:50 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,21 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, TextInput, View, Button, Alert, Pressable } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', senha: '' };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.boxEmail}>
+          <View style={styles.iconContainer}>
+            <Icon name="envelope" size={20} color="black" />
+          </View>
+          <TextInput
+            style={styles.inputEmail}
+            placeholder="Seu Email"
+            onChangeText={(email) => this.setState({ email })}
+            value={this.state.email}
+          />
+          <Text style={styles.textEmail}>
+            {ValidateEmail(this.state.email)}
+          </Text>
+        </View>
+        <View style={styles.boxEmail}>
+          <View style={styles.iconContainer}>
+            <Icon name="key" size={20} color="black" />
+          </View>
+          <TextInput
+            style={styles.inputEmail}
+            placeholder="Senha"
+            onChangeText={(senha) => this.setState({ senha })}
+            value={this.state.senha}
+
+          />
+        </View>
+        <View>
+          <Pressable
+            style={styles.pressed}
+
+            onPress={() => 
+              senhaAndEmail(this.state.senha, this.state.email)}
+          >
+            <Text style={styles.buttonText}>Entrar</Text>
+          </Pressable>
+        </View>
+
+      </View>
+    );
+  }
+}
+
+function senhaAndEmail(senha, email) {
+  console.log("senha ", senha, " email ", email);
+}
+
+function ValidateEmail(email) {
+  if (email.includes(".com") && email.includes("@") || !email) {
+    return "";
+  } else {
+    return "Email Invalido";
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pressed: {
+    backgroundColor: '#ddd',
+  },
+  boxEmail: {
+    alignItems: 'left',
+    justifyContent: 'left',
+  },
+  textEmail: {
+    padding: '10px',
+    fontSize: '15px',
+  },
+  inputEmail: {
+    width: '250px',
+    height: '40px',
+    borderWidth: '1px',
+    paddingLeft: '40px',
+    borderColor: 'black',
+    borderRadius: "5px",
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
 });
+
